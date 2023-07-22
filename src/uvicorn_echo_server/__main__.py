e5c034f217b44dd10da4d9c8e6ee8a57e07da2a3
Naoya Yamashita <conao3@gmail.com>
2023-07-22 18:01:42 +0900

    implement echo server

--- src/uvicorn_echo_server/__main__.py
+++ src/uvicorn_echo_server/__main__.py
@@ -1,9 +1,32 @@
+import logging
+import logging.config
+import pathlib
+
+import yaml
+
+
+with open(pathlib.Path(__file__).parent / 'logging.conf.yml') as f:
+    logging.config.dictConfig(yaml.safe_load(f))
+
+
+logger = logging.getLogger(__name__)
+
+
 def main() -> None:
-    print('try `poetry run uvicorn uvicorn_echo_server.__main__:app --reload`')
+    logger.info('try `poetry run uvicorn uvicorn_echo_server.__main__:app --reload`')
 
 
 async def app(scope, receive, send):
-    print(scope, receive, send)
+    logger.info(f'scope: {scope}')
+    logger.info(f'receive: {receive}')
+    logger.info(f'send: {send}')
+
+    body = b''
+    more_body = True
+    while more_body:
+        message = await receive()
+        body += message.get('body', b'')
+        more_body = message.get('more_body', False)
 
     await send({
         'type': 'http.response.start',
@@ -14,7 +37,7 @@ async def app(scope, receive, send):
     })
     await send({
         'type': 'http.response.body',
-        'body': b'Hello, world!',
+        'body': body or b'Hello, world!',
     })
 
 
