1579c686ad802b1d6c7b0838ae4d24b344741b16
Naoya Yamashita <conao3@gmail.com>
2023-07-22 17:45:20 +0900

    implement simple server

--- src/uvicorn_echo_server/__main__.py
+++ src/uvicorn_echo_server/__main__.py
@@ -1,2 +1,22 @@
 def main() -> None:
-    print('hello')
+    print('try `poetry run uvicorn uvicorn_echo_server.__main__:app --reload`')
+
+
+async def app(scope, receive, send):
+    print(scope, receive, send)
+
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [
+            [b'content-type', b'text/plain'],
+        ],
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': b'Hello, world!',
+    })
+
+
+if __name__ == '__main__':
+    main()
